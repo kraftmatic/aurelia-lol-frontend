e09291069c659a7ea1a5807ed1f3bd17c7da2eed
Nicholas Kraftor <nicholas.p.kraftor@ehi.com>
2015-09-20 22:24:56 -0500

    no message

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,10 @@
 // all gulp tasks are located in the ./build/tasks directory
 // gulp configuration is in files in ./build directory
 require('require-dir')('build/tasks');
+
+var gulp = require('gulp')
+var runSeq = require('run-sequence')
+
+gulp.task('heroku:production', function(){
+  runSeq('clean', 'build', 'minify')
+})
