013ead72a9e6398f496e1d045f37ff7962dbbae8
Nicholas Kraftor <nicholas.p.kraftor@ehi.com>
2015-09-20 22:32:52 -0500

    no message

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,11 @@
 // all gulp tasks are located in the ./build/tasks directory
 // gulp configuration is in files in ./build directory
 require('require-dir')('build/tasks');
+
+gulp.task('serveprod', function() {
+  connect.server({
+    root: [your_project_path],
+    port: process.env.PORT || 5000, // localhost:5000
+    livereload: false
+  });
+});
