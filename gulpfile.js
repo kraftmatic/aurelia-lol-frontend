ab9ed7051e022b72fcf10f48d5c59d68f3d82cfd
Nicholas Kraftor <nicholas.p.kraftor@ehi.com>
2015-09-21 22:56:21 -0500

    add klj;

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 // all gulp tasks are located in the ./build/tasks directory
 // gulp configuration is in files in ./build directory
-require('require-dir')('build/tasks');
+
 
 gulp.task('serveprod', function() {
   connect.server({
